c0c48f4bc6d1fbe3baecb3b749ea893fd787deca
Mike <micah41224@gmail.com>
2021-12-03 09:30:18 -0500

    Working on client folder

---
+++ client/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+const client = new ApolloClient({
+  uri: 'https://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
